b105e56e9068f39fb5b0fd3dc21b5095e96510e2
raflialamsyah666 <raflialamsyah@student.uir.ac.id>
2025-05-06 20:13:13 +0700

    Menambahkan javascript ke dalam projek vite

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,59 @@
 <script setup>
+import {ref ,computed, watch, onMounted} from 'vue'
 
+const tombol = ref(null)
+
+const teks = ref('')
+
+const lists = ref([
+  {id : 1, text : "Bangun", status : true},
+  {id : 2, text : "Mandi" , status : true},
+  {id : 3, text : "Sarapan", status : false}
+])
+
+function tambahkan(){
+  let isi = {
+    id : lists.value.length + 1,
+    text : teks.value,
+    status : false
+  }
+
+  lists.value.push(isi)
+  teks.value = ''
+}
+
+const selesai = computed(() => {
+  return lists.value.filter((x) => x.status == true)
+})
+
+const belum = computed(() => {
+  return lists.value.filter((x) => x.status == false)
+})
+
+const filter = ref('all')
+
+const filteredLists = computed(() => {
+  if (filter.value === 'done') {
+    return lists.value.filter(item => item.status === true)
+  } else if (filter.value === 'undone') {
+    return lists.value.filter(item => item.status === false)
+  } else {
+    return lists.value
+  }
+})
+
+const nomor = ref(2)
+
+watch(nomor , (a , b) => {
+  if(a > 16){
+  console.log("Data nya berubah dari "+b+" menjadi " + a)
+  nomor.value.setAttribute("disabled",true)
+  }
+})
+
+function hapus(id) {
+  lists.value = lists.value.filter(item => item.id !== id)
+}
 </script>
 
 <template>
